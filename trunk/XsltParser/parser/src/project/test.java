1604bdc5e22258375fe7c4840032194c512d5790
Adam.Sajdak <Adam.Sajdak@d50251c6-fe4d-0410-8778-f90e588951a5>
2008-06-05 15:31:18 +0000

    kod z netbeansa

---
+++ trunk/XsltParser/parser/src/project/test.java
@@ -0,0 +1,23 @@
+package project;
+import xslt.parser.*;
+import xslt.lexer.*;
+import xslt.node.*;
+import java.io.*;
+
+public class test { 
+	private static Translation translation;
+	public static void main(String[] arguments) 
+	{ 
+		try {
+// Create a Parser instance. 
+		Parser p =new Parser(new Lexer(new PushbackReader(new InputStreamReader(new FileInputStream ("test.xslt")), 1024)));
+
+// Parse the input. 
+		Start tree = p.parse();
+		translation=new Translation();
+		tree.apply(translation);
+		translation.getOutput();
+		//System.out.println(tree.toString());
+		}
+// Apply the translation. 
+		 catch(Exception e) { System.out.println(e.getMessage()); } } } 
